efac8347c960f8e984d6d5683e58a34fb2ab1292
Enrique Urbina <btace1347@yahoo.com>
2018-04-20 21:10:57 -0500

    added validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://Ace:Ace@ds149324.mlab.com:49324/social-media-app'
+    mongoURI: 'mongodb://Ace:Ace@ds149324.mlab.com:49324/social-media-app',
+    secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const post = require('./routes/api/post')
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const post = require('./routes/api/post');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 //init app
 const app = express();
 const db = require('./config/keys').mongoURI;
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //connect to mongoDB
 mongoose.connect(db)
     .then(() => console.log('Database Connected'))
     .catch(err => console.log(err));
 
-app.get('/', function (req, res) {
-    res.send('Server Running!');
-});
-//use routes
+//passport middleware
+app.use(passport.initialize());
 
+require('./config/passport' )(passport);
+
+
+//use routes
 app.use('/api/user', users );
 app.use('/api/profile', profile );
 app.use('/api/post', post );
